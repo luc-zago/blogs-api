7b640ba1edeb3e37bccc0de46be6f36a476be8b0
Lucas <luc.zago@hotmail.com>
2021-05-24 09:54:11 -0300

    Finalizado requisito 2

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const { userRoutes } = require('./routes');
 
 const app = express();
+app.use(express.json());
+app.use(userRoutes);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
